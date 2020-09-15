02890cd334afb637e7235f6799a33e80b6a2ef51
Jacob Sandström <jacob.sandstrom@Jacobs-MacBook-Pro.local>
2020-09-15 14:16:13 +0200

    supervisor# Please enter the commit message for your changes. Lines starting

--- server/lib/server/supervisor.ex
+++ server/lib/server/supervisor.ex
@@ -2,7 +2,20 @@ defmodule Server.Supervisor do
   alias Server.HttpServer
 
   def start(port) when is_integer(port) and port > 1023 do
-    server = spawn(fn -> HttpServer.start(port))
+    server = spawn(fn -> HttpServer.start(port) end)
+    |> IO.inspect()
+    |> life_checker(port)
+
   end
+
+  def life_checker(pid, port) do
+    :timer.sleep(1000)
+    Process.alive?(pid)
+    |> resurrect(pid, port)
+  end
+
+  def resurrect(true, pid, port), do: life_checker(pid, port)
+  def resurrect(false, _pid, port), do: spawn(fn -> start(port) end)
+
 
 end
